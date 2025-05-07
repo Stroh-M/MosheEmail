33619060c564549d61a5a98cf260c1fcec86f95d
Meir Stroh <Meir0609@outlook.com>
2025-05-07 11:49:41 -0400

    add loggin basic config

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-import proccess_emails
+import proccess_emails, logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(name)s - %(message)s',
+    handlers=[
+        logging.FileHandler("main.log"),
+        logging.StreamHandler()
+    ]
+)
 
 proccess_emails.main()
