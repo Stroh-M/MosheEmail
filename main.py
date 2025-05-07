b517960cf5c1f4ffc698bf49e4640d0047052a77
Meir Stroh <Meir0609@outlook.com>
2025-05-07 16:40:53 -0400

    add logging config

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import proccess_emails, logging
+import logging
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(name)s - %(message)s',
     handlers=[
-        logging.FileHandler("main.log"),
+        logging.FileHandler("main.log", encoding='utf-8'),
         logging.StreamHandler()
-    ]
+    ],
+    force=True
 )
 
-proccess_emails.main()
+import process_emails
+
+process_emails.main()
